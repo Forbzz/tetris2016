af805649d19edf12c04583d52baaba8ccb6fe055
Forbzz <ta@et.esiea.fr>
2016-04-06 15:17:13 +0200

    ajout Gameshow et scoreShow

---
+++ src/main/java/fr/esiea/tetris2016/view/GameShow.java
@@ -0,0 +1,120 @@
+package fr.esiea.tetris2016.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+import fr.esiea.tetris2016.model.Model;
+
+public class GameShow extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel[][] grid;
+	private Color[] color;
+	
+	public GameShow(){
+		
+		GridLayout layout = new GridLayout(16,10);
+		setLayout(layout);
+		
+		//initialisation des 8 couleurs
+		color= new Color[8];
+		this.assignColor();
+		
+		//Construction de la grille
+		grid= new JPanel[16][10];
+		this.buildGrid();
+		
+
+		
+	}
+	public JPanel[][] getGrid(){
+		
+		return this.grid;
+	}
+	
+	
+	
+	public Color getColor(int nb){
+		
+		return color[nb];
+	}
+	
+	public void assignColor(){
+		// Couleurs inspirés de "Tetris Friend"
+		color[0] = Color.white; // background
+		color[1] = Color.magenta; //T
+		color[2] = Color.cyan; // I
+		color[3] = Color.red; //Z
+		color[4] = Color.yellow; // O
+		color[5] = Color.green; //S
+		color[6] = Color.blue; //J
+		color[7] = Color.orange; //L
+		
+		
+		
+		
+	}
+	
+	public void buildGrid(){
+		
+		Dimension cellSize = new Dimension(35,35);
+		
+		for( int i = 0; i < 16; i++){
+			
+			for (int j = 0; j < 10 ; j++){
+				
+				grid[i][j] = new JPanel();
+				grid[i][j].setPreferredSize(cellSize);
+				grid[i][j].setBorder(BorderFactory.createLineBorder(Color.black));
+				add(grid[i][j]);
+			}
+			
+		}
+		
+		
+	}
+	
+	// met une couleur à tout ce qui est statique (background et pieces ancrées)
+	public void addColorBackground(int [][] grid){ 
+		
+		int nbCell=0; //numéro de la cellule
+	
+		for (int i = 0; i<16; i++) {
+			for (int j = 0; j<10; j++) {
+				nbCell = grid[i][j];
+				this.grid[i][j].setBackground(color[nbCell]);
+			}
+		}
+	
+	}
+	
+	
+	public void addColorPieces( int [][] grid, int [][] instancedPiece, int[] currentPiecePos, int currentPieceRot){
+		
+
+		int nbCell = 0;
+		int count=0;
+		for (int i=0; i < 4; i++) {
+			for (int j=0; j < 4; j++) {
+					nbCell= instancedPiece[currentPieceRot][count];
+					if (nbCell>0 ) {
+		
+						this.grid[currentPiecePos[0]+i][currentPiecePos[1]+j].setBackground(color[nbCell]);
+					}
+					System.out.println("count=  " + i + j + count);
+					count++;
+				}
+			
+			}
+		}
+	}
+		
+	
+
---
+++ src/main/java/fr/esiea/tetris2016/view/ScoreShow.java
@@ -0,0 +1,76 @@
+package fr.esiea.tetris2016.view;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.Arrays;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class ScoreShow extends JLabel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JLabel currentScore;
+	private JLabel title;
+	private JLabel ladder;
+	public ScoreShow(){
+		
+		GridLayout layout = new GridLayout(4,1);
+		setLayout(layout);
+		this.title = new JLabel("SCORE", SwingConstants.CENTER);
+		this.currentScore = new JLabel("0", SwingConstants.CENTER);
+		this.ladder = new JLabel("ranking", SwingConstants.CENTER);
+		title.setPreferredSize(new Dimension(150,30));
+		currentScore.setPreferredSize(new Dimension(100,30));
+		add(title);
+		add(currentScore);	
+		
+		
+		
+	}
+
+
+	public void actualScore(int score){
+System.out.println("test KZO");
+		
+		currentScore.setText(Integer.toString(score));
+		
+		currentScore.setVisible(false);
+		currentScore.setVisible(true);
+	}
+	
+	public String getCurrentScore(){
+		
+		return currentScore.getText();
+	}
+	
+	public String getTitle(){
+		return title.getText();
+	}
+	
+	public String getLadder(){
+		
+		return ladder.getText();
+	}
+	
+	public void showEnd(int score, int [] scoreArray){
+	
+		title.setText("YOU LOST");
+		currentScore.setText("Your score is: "+Integer.toString(score));
+		
+		title.setVisible(false);
+		title.setVisible(true);
+		
+		currentScore.setVisible(false);
+		currentScore.setVisible(true);;
+		
+		ladder.setText("HIGHSCORE:" +Arrays.toString(scoreArray));
+		
+		
+	}
+	
+	
+}
