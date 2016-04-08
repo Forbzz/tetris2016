ff2bf31fc0df5c93657eddb47b4dbe0ef9af3393
Forbzz <ta@et.esiea.fr>
2016-04-08 03:50:00 +0200

    Ajout des dependances et des tests

--- src/main/java/fr/esiea/tetris2016/Main.java
+++ src/main/java/fr/esiea/tetris2016/Main.java
@@ -7,10 +7,15 @@ import fr.esiea.tetris2016.controller.Controller;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		
+		
+		
 		System.out.println("Lancement du Tetris");
 		//façade cachant le controlleur:	
 		Controller hiddenController = new Controller();
 		hiddenController.run();
 	}
+	
+	
 
 }
